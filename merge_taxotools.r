0231613302546f234fe185a7828bf03ff70d644c
Teresa Mayfield-Meyer <jegelewicz66@gmail.com>
2021-04-27 16:32:56 -0600

    tweaks

--- merge_taxotools.r
+++ merge_taxotools.r
@@ -15,24 +15,11 @@ FMNH_taxo <- DwC2taxo(FMNH_species, source = "FMNH")
 Lewis <- read.csv("~/GitHub/tpt-siphonaptera/output/Lewis_DwC.csv", na = "NA") # read in cleaned Lewis review file
 Lewis_ht <- higer_taxa(Lewis, Lewis$taxonRank) # FMNH higher taxa
 Lewis_species <- species(Lewis, Lewis$taxonRank) # FMNH species taxa
-Lewis_taxo <- DwC2taxo(Lewis_species)
+Lewis_taxo <- DwC2taxo(Lewis_species, source = "Lewis")
 
 CoL <- read.csv("~/GitHub/tpt-siphonaptera/output/CoL_DwC.csv", na = "NA") # read in cleaned Lewis review file
 CoL_ht <- higer_taxa(CoL, CoL$taxonRank) # CoL higher taxa
 CoL_species <- species(CoL, CoL$taxonRank) # CoL species taxa
-Col_taxo <- DwC2taxo(CoL_DwC)
+Col_taxo <- DwC2taxo(CoL_DwC, source = "CoL")
 
-dwc_siphonaptera <- rbindlist(list(NMNH_Siphonaptera, FMNH_Siphonaptera, Lewis_Siphonaptera, CoL_DwC), fill = TRUE) # combine all DarwinCore files
-
-taxo_siphonaptera <- dwc_siphonaptera
-
-colnames(taxo_siphonaptera) <- as.character(df1[,2])
-
-taxo_siphonaptera <- rename(taxo_siphonaptera,
-                   replace = c("id" = "taxonID",
-                               "source" = "TPTdataset",
-                               "accid" = "acceptedNameUsageID",
-                               "species" = "specificEpithet",
-                               "subspecies" = "infraspecificEpithet",
-                               "author" = "scientificNameAuthorship",
-                               "taxonlevel" = "taxonRank"))
+taxo_siphonaptera <- rbindlist(list(NMNH_taxo, FMNH_taxo, Lewis_taxo, CoL_taxo), fill = TRUE) # combine all taxo files
